580aca05028ea995326bc1746afabd94c8359ff2
Victor Prado <prado.victor1005@gmail.com>
2022-10-06 13:22:38 -0300

    alterando porta

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const routes = require('./routes');
 
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3003;
 
 app.use(express.json());
 app.use(cors)
